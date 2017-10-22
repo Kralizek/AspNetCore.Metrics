bd4a4285346baf119c131c23a1d554afebf91e42
Renato Golia <golia.renato@gmail.com>
2017-10-22 16:17:02 +0200

    Added ASP.NET Core MVC sample

---
+++ samples/CloudWatchSample/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Kralizek.AspNetCore.Metrics;
+using Amazon.CloudWatch;
+
+namespace SampleWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddMvcDimensions();
+
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+
+            services.AddAWSService<IAmazonCloudWatch>();
+
+            services.PersistMetricsOnCloudWatch(
+                new CloudWatchMetric("Elapsed time by Server", MetricValues.ElapsedTime, MetricDimensions.MachineName) { Unit = StandardUnit.Milliseconds },
+                new CloudWatchMetric("Elapsed time by Controller/Action", MetricValues.ElapsedTime, AspNetCoreMvcMetricDimensions.ControllerName, AspNetCoreMvcMetricDimensions.ActionName) { Unit = StandardUnit.Milliseconds },
+                new CloudWatchMetric("Elapsed time by HTTP method", MetricValues.ElapsedTime, MetricDimensions.HttpMethod) { Unit = StandardUnit.Milliseconds },
+                new CloudWatchMetric("Elapsed time", MetricValues.ElapsedTime) { Unit = StandardUnit.Milliseconds },
+                new CloudWatchMetric("Elapsed time by Controller/Action/HTTP status", MetricValues.ElapsedTime, AspNetCoreMvcMetricDimensions.ControllerName, AspNetCoreMvcMetricDimensions.ActionName, MetricDimensions.HttpResponseStatus) { Unit = StandardUnit.Milliseconds }
+            );
+
+            services.Configure<CloudWatchMetricPersisterConfiguration>(cfg => cfg.Namespace = "Sample Kralizek.AspNetCore.Metrics");
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMetricsCollector();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

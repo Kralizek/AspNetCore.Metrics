ca4d7dc55e3502159fa65a64fbd3c5a373461dc3
Renato Golia <golia.renato@gmail.com>
2017-10-26 17:08:52 +0200

    Created TestBase.XUnit

---
+++ tests/TestBase.XUnit/InlineAutoMoqDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace Tests
+{
+    public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoMoqDataAttribute(params object[] arguments) : base (new Fixture().Customize(new AutoMoqCustomization()), arguments)
+        {
+
+        }
+    }
+}

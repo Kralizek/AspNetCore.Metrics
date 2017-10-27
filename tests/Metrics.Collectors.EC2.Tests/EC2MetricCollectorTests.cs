bf1876f1c0b339dda043ff793aa4481e1d150845
Renato Golia <golia.renato@gmail.com>
2017-10-27 16:49:39 +0200

    Fixes to tests

--- tests/Metrics.Collectors.EC2.Tests/EC2MetricCollectorTests.cs
+++ tests/Metrics.Collectors.EC2.Tests/EC2MetricCollectorTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using Xunit;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Tests
@@ -11,16 +12,18 @@ namespace Tests
         [Theory, AutoMoqData]
         public async Task Dimensions_are_added_if_in_AWS(EC2MetricCollector sut, IAWSMetadataProvider provider, IDictionary<IMetricDimension, object> dimensions, IDictionary<IMetricValue, IValue> metrics)
         {
-            Mock.Get(provider).Setup(p => p.IsInAWS()).Returns(true);
+            Mock.Get(provider).Setup(p => p.IsInAWS())
+                .Callback(() => {})
+                .Returns(true);
 
             AWSMetadataProvider.SetInstance(provider);
 
             await sut.OnActionExecutingAsync(dimensions, metrics);
 
-            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.InstanceId));
-            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.AmiId));
-            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.AvailabilityZone));
-            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.InstanceType));
+            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.InstanceId), "Dimensions does not contain InstanceId");
+            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.AmiId), "Dimensions does not contain AmiId");
+            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.AvailabilityZone), "Dimensions does not contain AvailabilityZone");
+            Assert.True(dimensions.ContainsKey(EC2MetadataDimensions.InstanceType), "Dimensions does not contain InstanceType");
         }
 
         [Theory, AutoMoqData]
--- tests/TestBase.XUnit/InlineAutoMoqDataAttribute.cs
+++ tests/TestBase.XUnit/InlineAutoMoqDataAttribute.cs
@@ -4,11 +4,11 @@ using AutoFixture.Xunit2;
 
 namespace Tests
 {
-    public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
-    {
-        public InlineAutoMoqDataAttribute(params object[] arguments) : base (new Fixture().Customize(new AutoMoqCustomization()), arguments)
-        {
+    //public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+    //{
+    //    public InlineAutoMoqDataAttribute(params object[] arguments) : base (new Fixture().Customize(new AutoMoqCustomization()), arguments)
+    //    {
 
-        }
-    }
+    //    }
+    //}
 }
